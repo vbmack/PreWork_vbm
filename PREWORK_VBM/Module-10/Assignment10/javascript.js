241e3f065dea20403916a4de658bd30872ab1855
Victor Mack <vbmack@uncc.edu>
2016-11-02 14:38:53 -0400

    Add files via upload

---
+++ PREWORK_VBM/Module-10/Assignment10/javascript.js
@@ -0,0 +1,24 @@
+
+
+document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+   });
+
+document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "125px";{
+            document.getElementById("box").style.backgroundColor=blue;
+
+           
+ }});          
+
+
+ 
+
+
+ document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+               });
